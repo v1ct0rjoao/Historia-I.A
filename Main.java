88df74d44e215b06d4126d6a4cc1c63e1d654eab
v1ct0rjoao <joao.victormeneses@ufrpe.br>
2022-11-22 14:52:24 -0300

    Historia commit 1

--- Main.java
+++ Main.java
@@ -4,11 +4,55 @@ class Main{
 
 public static void main(String[] argas) {
 
+String PrimeiroCapitulo = "---- Reconhecimento ----\n\n"+
+"Olá recruta me chamo Alan sou a inteligencia artificial\n"+
+"responsavel testar suas habilidades fisicas e mentais\n"+
+"O primeiro teste é de coragem, bem simples, espero que se\n"+
+"saia bem\n\n"+
+">>>>Você é levado até um terrraço<<<<\n\n"+
+">>> Alan - Para conseguir consquistar o objetivo nesse teste é preciso que salte do terraço\n\n"+
+"Você olha para baixo e não percebe fim, o local parece um\n"+
+"abismo.\n"+
+">>> Alan - Você tem a opção de saltar ou desistir, qual você escolher?";
 
+String Escolha1PrimieiroCapitulo= "saltar";
+String Escolha2PrimieiroCapitulo = " Desitir";
 
+String SegundoCapitulo =  "---- O bom recruta ----\n\n"+
+"Mesmo em pânico, sentidos confusos, você salta do terraço.\n"+
+"Afinal, você nasceu para ser um soldado. Quando você pula\n"+
+"senti a sensação que aquele abismo não tem fim, até que alguns\n"+
+"feixes de luz, começam a sair do chão.\n\n"+
+">>> Alan - Meus parabéns recruta, nem questionou as ordens, passou no primeiro teste.\n\n"+
+"quando você ver o Alan,percebe que tudo aquilo se desmanchava e no final, você\n"+
+"estava dentro de um tipo de ilusão\n\n"+
+">>> Alan - isso tudo era apenas um cenario holografico,\n"+
+"seu segundo teste, vai ser mais dificil, pois vão ser dois testes\n"+
+"simultaneos, você e outro recruta.\n\n"+
+"é colocado uma caixa proxima a vocês dois, com duas armas\n\n"+
+">>> Alan - essas duas armas funcionam eletronicamente, elas só\n"+
+"desarmam se foram apertado o gatilho ao mesmo tempo, vocês tem duas opcôes\n"+
+"atirar um no outro ao mesmo tempo e não morrer e ser desqualificado ou apenas um\n"+
+"atirar e ser aprovado no teste\n\n"+
+"Vocês estão um em frente o outro\n"+
+"- Atirar ao mesmo tempo\n\n - Apenas Recruta 1 atira \n\n - Apenas Recruta 2 atira";
 
+String Escolha1SegundoCapitulo = "Atirar ao mesmo tempo";
+String Escolha2SegundoCapitulo = "Apenas Recruta 1 atira";
+String Escolha3SegundoCapitulo = "Apenas Recruta 2 atira";
 
+String TerceitoCapitulo = "---- FRACASSADO ----\n\n"+
+"Você entra em pânico e mesmo você querendo ir o seu corpo\n"+
+"não te obedece e infelizmente desisti de saltar\n\n"+
+">>> Alan - é uma pena não conseguir, seu pai tinha muita expectativa sobre você recruta\n"+
+"mas tudo bem os medrosos e covardes podem viver em paz, junto com os sem facções\n"+
+"espero que sobreviva sozinho, pode seguir estes homens.\n\n"+
+"Dois homens surgem do seu lado e te levam até saída e te jogam, como lixo na rua\n"+
+"em meio aos drogados";
 
+String QuartoCapitulo = "";
+
+System.out.println(TerceitoCapitulo);
 
 
 
