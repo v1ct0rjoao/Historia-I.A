7c678b30a418e0ff3bfd8e4fc28991c25995359c
v1ct0rjoao <joao.victormeneses@ufrpe.br>
2022-11-22 15:31:36 -0300

    2 commit, interação de condição

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+
 import java.util.Scanner;
 
 class Main{
 
 public static void main(String[] argas) {
+
+Scanner leitor = new Scanner(System.in);
+String Resposta;
 
 String PrimeiroCapitulo = "---- Reconhecimento ----\n\n"+
 "Olá recruta me chamo Alan sou a inteligencia artificial\n"+
@@ -13,10 +17,12 @@ String PrimeiroCapitulo = "---- Reconhecimento ----\n\n"+
 ">>> Alan - Para conseguir consquistar o objetivo nesse teste é preciso que salte do terraço\n\n"+
 "Você olha para baixo e não percebe fim, o local parece um\n"+
 "abismo.\n"+
-">>> Alan - Você tem a opção de saltar ou desistir, qual você escolher?";
+">>> Alan - Você tem a opção de saltar ou desistir, qual você escolher?\n\n"+
+"- saltar \n\n- desistir\n\n"+
+"DIGITE: ";
 
 String Escolha1PrimieiroCapitulo= "saltar";
-String Escolha2PrimieiroCapitulo = " Desitir";
+String Escolha2PrimieiroCapitulo = "desistir";
 
 String SegundoCapitulo =  "---- O bom recruta ----\n\n"+
 "Mesmo em pânico, sentidos confusos, você salta do terraço.\n"+
@@ -34,13 +40,14 @@ String SegundoCapitulo =  "---- O bom recruta ----\n\n"+
 "desarmam se foram apertado o gatilho ao mesmo tempo, vocês tem duas opcôes\n"+
 "atirar um no outro ao mesmo tempo e não morrer e ser desqualificado ou apenas um\n"+
 "atirar e ser aprovado no teste\n\n"+
-"Vocês estão um em frente o outro\n"+
-"- Atirar ao mesmo tempo\n\n - Apenas Recruta 1 atira \n\n - Apenas Recruta 2 atira";
+"Vocês estão um em frente o outro:\n\n"+
+"- Atirar ao mesmo tempo\n\n - Apenas Recruta 1 atira \n\n - Apenas Recruta 2 atira\n\n"+
+"DIGITE:";
 
-String Escolha1SegundoCapitulo = "Atirar ao mesmo tempo";
-String Escolha2SegundoCapitulo = "Apenas Recruta 1 atira";
-String Escolha3SegundoCapitulo = "Apenas Recruta 2 atira";
-
+String Escolha1SegundoCapitulo = "atirar juntos";
+String Escolha2SegundoCapitulo = "apenas recruta 1 atira";
+String Escolha3SegundoCapitulo = "apenas recruta 2 atira";
+//Esse capitulo se refere a segunda escolha do primeiro capitulo
 String TerceitoCapitulo = "---- FRACASSADO ----\n\n"+
 "Você entra em pânico e mesmo você querendo ir o seu corpo\n"+
 "não te obedece e infelizmente desisti de saltar\n\n"+
@@ -49,100 +56,66 @@ String TerceitoCapitulo = "---- FRACASSADO ----\n\n"+
 "espero que sobreviva sozinho, pode seguir estes homens.\n\n"+
 "Dois homens surgem do seu lado e te levam até saída e te jogam, como lixo na rua\n"+
 "em meio aos drogados";
-
-String QuartoCapitulo = "";
-
-System.out.println(TerceitoCapitulo);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//se refere a Escolha1SegundoCapitulo
+String QuartoCapitulo = "---- Um Voto de amizade ----\n\n"+
+"Vocês dois não se conhecem, mas como irmaõs de guerra, decidem usar\n"+
+"as armas ao mesmo, miram um no outro e quando clicam no gatilho sai confete\n\n"+
+">>>> Alan - Parabéns, vocês são oficialmente soldados da ARQUIA\n"+
+"aqui dentro devem sempre manter esses principios, nunca deixem um amigo para trás\n\n"+
+"Recruta 1 e Recruta 2 são classificados como guerreiros da paz da ARQUIA, serão responsave´s\n"+
+"por liderar chamados de paz em outros países secretamente\n\n"+
+"FIM DO LIVRO";
+//se refere a Escolha2SegundoCapitulo
+String QuintoCapitulo = "---- O mal soldado ----\n\n"+
+"Recruta não se preocupa com o seu companheiro, o objetivo é\n"+
+"passar em todos os testes, mesmo recruta 2 tentando conversar ele a não\n"+
+"fazer isso ele aperta o gatilho, quando ele faz isso sai confete da sua arma\n"+
+"Alan surge decepicionado.\n\n"+
+">>>> Alan - Você nunca será um soldado, ele era seu aliado\n"+
+"isso era apenas um testem, infelizmente apenas o recruta 2 passou\n\n"+
+"Recruta 1 é mandado embora...\n\n"+
+"FIM DO LIVRO";
+
+String SextoCapitulo = "---- Diplomata ----\n\n"+
+"você conversa com o recruta 2, explica um plano\n\n"+
+">>>> Recruta 1 - atira em mim, não tem bala dentro, eu tenho certeza\n"+
+"eles nunca iam matar dois recrutas.\n\n"+
+">>>> Recruta 2 - Você é maluco, mas só quem sai ganhando sou mesmo\n"+
+"agora se você morrer a culpa é sua!!\n\n"+
+"quando o recruta 2 puxa o gatilho percebe que recruta 1 estava certo\n"+
+"Alan aparece em um monitor grande dentro da sala\n\n"+
+">>>> Alan - Parabéns Recrutas, confiança, estrategia e diplomacia\n"+
+"tudo isso cria um bom soldado, os dois estão aprovados, vocês fazem parte\n"+
+"da ARQUIA e vão fazer parte das forças especias de solução de guerras internacionais\n\n"+
+"FIM DO LIVRO";
+
+
+System.out.println(PrimeiroCapitulo);
+Resposta = leitor.nextLine();
+
+
+      if(Resposta.equals(Escolha1PrimieiroCapitulo))
+       {
+         System.out.println(SegundoCapitulo);
+         Resposta = leitor.nextLine();
+         if(Resposta.equals(Escolha1SegundoCapitulo))
+         {
+            System.out.println(QuartoCapitulo);
+         }else if(Resposta.equals(Escolha2SegundoCapitulo))
+         {
+            System.out.println(QuintoCapitulo);
+         }else if(Resposta.equals(Escolha3SegundoCapitulo))
+         {
+            System.out.println(SextoCapitulo);
+         }
+       }
+
+       if(Resposta.equals(Escolha2PrimieiroCapitulo))
+       {
+        System.out.println(TerceitoCapitulo);
+        Resposta = leitor.nextLine();
+       }
+      
+  }
 
 }
