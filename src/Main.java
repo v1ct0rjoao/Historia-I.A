4c08710066e4ee6735061c2491da3dd115ee0ead
joao <joao.victormeneses@ufrpe.br>
2023-04-25 13:44:53 -0300

    javaFX

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-class Main {
 
+class Main extends Application {
 
     public static void main(String[] args) throws FileNotFoundException {
     
@@ -13,7 +15,16 @@ class Main {
     HashMap<String,Personagem> personagens =  controle.receberPersonagem("rsc/ArquivoDosPersonagens.txt");
     HashMap<String,Capitulo> capitulos =  controle.receberCapitulos("rsc/ArquivoDosCapitulos.txt", personagens, scan );
  
+
+    launch();
+    
      Capitulo raiz = capitulos.get("A Figura Misteriosa");
      raiz.executar();
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 }
